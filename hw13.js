f18302177b4dfaae67e0fa2ad3aad0fcd0792ff5
Egor-Goloshchapov <141495264+Egor-Goloshchapov@users.noreply.github.com>
2024-11-16 07:20:47 +0200

    Add files via upload

---
+++ hw13.js
@@ -0,0 +1,69 @@
+const user = {
+    mod: "happy",
+    hobby: "skydiving",
+    premium: false,
+};
+
+
+const { mod, hobby, premium } = user;
+console.log(mod, hobby, premium); 
+
+
+const products = {
+    name: "Smith",
+    age: 33,
+    length: 183,
+    job: "Web developer"
+};
+
+const { name, age, length, job } = products;
+console.log(name, age, length, job);
+
+const salary = {
+    Mark: 1500,
+    Max: 1050,
+    Alex: 5500,
+};
+
+const { Mark, Max, Alex } = salary;
+console.log(Mark, Max, Alex);
+
+
+
+
+const employees = {
+    Alexander: 3,
+    Sascha: 7,
+    Ivan: 5,
+};
+
+const { Alexander, Sascha, Ivan } = employees;
+console.log(Alexander, Sascha, Ivan);
+
+
+
+const bankAccount = {
+    ownerName: "Іван Іванов",
+    accountNumber: "1234567890",
+    balance: 1000,
+};
+
+const { ownerName, accountNumber, balance } = bankAccount;
+console.log(ownerName, accountNumber, balance); 
+
+
+
+
+const user2 = {
+    name1: "Олена Петрівна",
+    email: "elena@example.com",
+    password: "123456",
+};
+
+const { name1, email, password } = user;
+console.log(name, email, password); 
+
+
+
+
+
